761a2b861be9e49b15b2168f9b0a887d8c81a0ac
huoyinghui <2285020853@qq.com>
2018-06-19 11:33:25 +0800

    add: tow sum

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -21,3 +21,27 @@ def two_sum(array, target):
         else:
             dic[target - num] = i
     return None
+
+
+def two_sum_v2(array, target):
+    target_dict = {}
+
+    for index, item in enumerate(array):
+        other_numb = target - item
+        if other_numb in target_dict:
+            return target_dict[other_numb], index
+        else:
+            target_dict[item] = index
+    return None
+
+
+def main():
+    nums = [2, 7, 11, 15]
+    target = 17
+    # ret = two_sum(nums, target)
+    ret = two_sum_v2(nums, target)
+    print(ret)
+
+
+if __name__ == '__main__':
+    main()
